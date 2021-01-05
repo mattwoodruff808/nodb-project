67902e48e4ba567b4162cbee4077bffec8bfb8a3
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-05 15:21:15 -0700

    Made more crucial CSS changes

--- src/Components/Hoid.js
+++ src/Components/Hoid.js
@@ -32,26 +32,28 @@ class Hoid extends Component {
         const {currentStory} = this.props;
 
         return (
-            <section className="Hoid-info">
+            <section className="Hoid-sidebar">
                 <h2>Bartender</h2>
-                <img src='https://i.pinimg.com/originals/80/ef/91/80ef9128f7171d1172de2dca678bdcc9.png' alt='Hoid playing his flute'/>
-                <p>Artist: Tara Spruit - (taratjah)</p>
-                <p>Name: Hoid</p>
-                <p>Ability: Everything</p>
-                {editView 
-                ? (
-                    <section>
-                        <input value={editStoryInp} 
-                               placeholder='Enter New Story' 
-                               onChange={(e) => this.handleStoryInp(e)}/>
-                        <button onClick={() => this.handleChangeStory()}>Begin Story</button>
-                    </section>
-                )
-                : (
-                    <section className="Click"> 
-                        <p onClick={() => this.handleToggleView()}>Story: {currentStory}</p>
-                    </section>
-                )}
+                <img className="Hoid-margin" src='https://i.pinimg.com/originals/80/ef/91/80ef9128f7171d1172de2dca678bdcc9.png' alt='Hoid playing his flute'/>
+                <p className="Artist-2">Artist: Tara Spruit - (taratjah)</p>
+                <section className="Hoid-info">
+                    <p>Name: Hoid</p>
+                    <p>Ability: Everything</p>
+                    {editView 
+                    ? (
+                        <section>
+                            <input value={editStoryInp} 
+                                placeholder='Enter New Story' 
+                                onChange={(e) => this.handleStoryInp(e)}/>
+                            <button onClick={() => this.handleChangeStory()}>Begin Story</button>
+                        </section>
+                    )
+                    : (
+                        <section className="Click"> 
+                            <p onClick={() => this.handleToggleView()}>Story: {currentStory}</p>
+                        </section>
+                    )}
+                </section>
             </section>
         )
     }
